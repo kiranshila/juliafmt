4128d31227feabf7542fb2330a2ce9ab86fe4995
Kiran Shila <me@kiranshila.com>
2022-01-20 18:12:22 -0800

    More work on the grammar

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,27 @@
 extern crate clap;
 extern crate pest;
+#[macro_use]
 extern crate pest_derive;
 
 use std::{fs, path};
 
-#[derive(clap::Parser, Debug)]
+use clap::Parser as ClapParser;
+use pest::Parser as PestParser;
+
+#[derive(ClapParser, Debug)]
 #[clap(author,version,about,long_about = None)]
 struct Args {
     #[clap(short, long)]
     path: path::PathBuf,
 }
 
-#[derive(pest_derive::Parser)]
+#[derive(Parser)]
 #[grammar = "parser/julia.pest"]
-pub struct JuliaParser;
+struct JuliaParser;
 
 fn main() {
     let args = Args::parse();
-    let contents = fs::read_to_string(args.path).unwrap();
+    let contents = fs::read_to_string(&args.path).unwrap();
     let result = JuliaParser::parse(Rule::program, &contents);
-    println!("{:?}", args.path)
+    println!("{:?}", result.unwrap())
 }
