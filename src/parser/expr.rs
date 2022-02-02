b095608e4824cafa087a12e441dc843e11f682e4
Kiran Shila <me@kiranshila.com>
2022-02-02 11:15:57 -0800

    Refactoring expr

--- src/parser/expr.rs
+++ src/parser/expr.rs
@@ -1,3 +1,4 @@
+use super::marker::CompletedMarker;
 use super::Parser;
 use crate::lexer::RawToken;
 
@@ -25,6 +26,10 @@ enum PrefixOp {
     Plus,
     Not,
     Radical,
+}
+
+enum PostfixOp {
+    Tick,
 }
 
 // Precedence and associativity from
@@ -69,50 +74,12 @@ pub(super) fn expr(p: &mut Parser) {
 }
 
 fn expr_binding_power(p: &mut Parser, minimum_binding_power: u8) {
-    // All the things that can be on either side of a binary operator
-    let mut lhs = match p.peek() {
-        Some(RawToken::Identifier)
-        | Some(RawToken::Integer)
-        | Some(RawToken::Hex)
-        | Some(RawToken::Octal)
-        | Some(RawToken::Binary)
-        | Some(RawToken::Float)
-        | Some(RawToken::Exponential) => {
-            let m = p.start();
-            p.bump();
-            m.complete(p, RawToken::Literal)
-        }
-        // Prefix operators
-        Some(RawToken::Plus) | Some(RawToken::Not) | Some(RawToken::Radical) => {
-            let op = match p.peek() {
-                Some(RawToken::Plus) => PrefixOp::Plus,
-                Some(RawToken::Not) => PrefixOp::Not,
-                Some(RawToken::Radical) => PrefixOp::Radical,
-                _ => return, // we’ll handle errors later.
-            };
-            let m = p.start();
-            let ((), right_binding_power) = op.binding_power();
-            // Eat the token
-            p.bump();
-
-            expr_binding_power(p, right_binding_power);
-            m.complete(p, RawToken::UnaryExpr)
-        }
-        // Parentheticals
-        Some(RawToken::LParen) => {
-            let m = p.start();
-            // Consume the LParen
-            p.bump();
-            // A paren "resets" operator precedence, so we can just recur
-            expr_binding_power(p, 0);
-            // Parens must have pairs
-            assert_eq!(p.peek(), Some(RawToken::RParen));
-            // Consume the closing paren
-            p.bump();
-            m.complete(p, RawToken::Parenthetical)
-        }
-        _ => unreachable!(),
+    let mut lhs = if let Some(lhs) = lhs(p) {
+        lhs
+    } else {
+        return; //Handle errors?
     };
+
     loop {
         // Grab from the precedence table
         let op = match p.peek() {
@@ -151,4 +118,52 @@ fn expr_binding_power(p: &mut Parser, minimum_binding_power: u8) {
         expr_binding_power(p, right_binding_power);
         lhs = m.complete(p, RawToken::BinaryExpr);
     }
+}
+
+fn lhs(p: &mut Parser) -> Option<CompletedMarker> {
+    // All the things that can be on either side of a binary operator
+    let cm = match p.peek() {
+        Some(RawToken::Identifier)
+        | Some(RawToken::Integer)
+        | Some(RawToken::Hex)
+        | Some(RawToken::Octal)
+        | Some(RawToken::Binary)
+        | Some(RawToken::Float)
+        | Some(RawToken::Exponential) => {
+            let m = p.start();
+            p.bump();
+            m.complete(p, RawToken::Literal)
+        }
+        // Prefix operators
+        Some(RawToken::Plus) | Some(RawToken::Not) | Some(RawToken::Radical) => {
+            let op = match p.peek() {
+                Some(RawToken::Plus) => PrefixOp::Plus,
+                Some(RawToken::Not) => PrefixOp::Not,
+                Some(RawToken::Radical) => PrefixOp::Radical,
+                _ => return None, // we’ll handle errors later.
+            };
+            let m = p.start();
+            let ((), right_binding_power) = op.binding_power();
+            // Eat the token
+            p.bump();
+
+            expr_binding_power(p, right_binding_power);
+            m.complete(p, RawToken::UnaryExpr)
+        }
+        // Parentheticals
+        Some(RawToken::LParen) => {
+            let m = p.start();
+            // Consume the LParen
+            p.bump();
+            // A paren "resets" operator precedence, so we can just recur
+            expr_binding_power(p, 0);
+            // Parens must have pairs
+            assert_eq!(p.peek(), Some(RawToken::RParen));
+            // Consume the closing paren
+            p.bump();
+            m.complete(p, RawToken::Parenthetical)
+        }
+        _ => unreachable!(),
+    };
+    Some(cm)
 }
